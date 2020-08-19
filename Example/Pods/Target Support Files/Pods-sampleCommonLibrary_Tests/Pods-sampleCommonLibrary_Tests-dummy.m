a0077ab41a903f1b7e4c0481182e955c230f7862
Bruno Henrique <brunohbr123@gmail.com>
2020-08-19 10:37:38 -0300

    first commit

---
+++ Example/Pods/Target Support Files/Pods-sampleCommonLibrary_Tests/Pods-sampleCommonLibrary_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sampleCommonLibrary_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_sampleCommonLibrary_Tests
+@end
---
+++ Example/Pods/Target Support Files/sampleCommonLibrary/sampleCommonLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_sampleCommonLibrary : NSObject
+@end
+@implementation PodsDummy_sampleCommonLibrary
+@end
